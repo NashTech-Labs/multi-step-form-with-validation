847c060e4774d4c6c659e953497ad4a9259bcc06
Neha Kumari <neha.kumari-ext@jci.com>
2023-11-16 14:41:13 +0530

    multi-step form with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'multi-step-form';
+  isLinear = true;
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.firstFormGroup = this.fb.group({
+      name: ['', Validators.required],
+      address: ['', Validators.required],
+      age: []
+    });
+
+    this.secondFormGroup = this.fb.group({
+      amount: ['', Validators.required],
+      stock: ['', Validators.required],
+    });
+  }
+
+  submit() {
+    console.log(this.firstFormGroup.value);
+    console.log(this.secondFormGroup.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import {MatInputModule} from '@angular/material/input';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatListModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
